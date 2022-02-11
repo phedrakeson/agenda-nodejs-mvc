599c909e7d9c1d3b52247250feb18d8d36ba7799
Gabriel Duarte <gabrield.developer@gmail.com>
2022-02-10 22:23:55 -0300

    implementa rota de registro de usuario, cria model, adiciona tratamento de erros

--- routes.js
+++ routes.js
@@ -7,5 +7,6 @@ route.get('/', homeController.index);
 
 // Rotas de Login
 route.get('/login', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
---
+++ src/models/LoginModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const LoginModel = new mongoose.model('login', LoginSchema);
+
+class Login {
+    constructor(body) {
+      this.body = body;
+      this.errors = [];
+      this.user = null;
+    }
+
+    async register() {
+      this.validar();
+      if(this.errors.length > 0) return;
+
+      try {
+        this.user = await LoginModel.create(this.body);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    validar() {
+      this.cleanUp();
+
+      if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+      if(this.body.password.length < 3 || this.body.password.length > 50) this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+
+
+    }
+
+    cleanUp() {
+      for(const chave in this.body) {
+        if(typeof this.body[chave] !== 'string') {
+          this.body[chave] = '';
+        }
+      }
+      this.body = {
+        email: this.body.email,
+        password: this.body.password
+      };
+    }
+}
+
+module.exports = Login;
